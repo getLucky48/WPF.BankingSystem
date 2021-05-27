516fcdd9067a53fe26323bc201a4db94f3c270de
Artyom <kerambit.c@yandex.ru>
2021-05-27 13:25:40 +0300

    Initial commit

---
+++ Lib/DBUtils.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Lib
+{
+    public class DBUtils
+    {
+
+        private static string host = "remotemysql.com";
+        private static string database = "HEtUgcXWv8";
+        private static string userid = "HEtUgcXWv8";
+        private static string password = "k4Aj1Xgz6C";
+
+        public static MySqlConnection getConnection()
+        {
+
+            string connString = "Server=" + host + ";Database=" + database + ";User Id=" + userid + ";password=" + password + "; Allow User Variables=True";
+
+            return new MySqlConnection(connString);
+
+        }
+
+        public static void execQuery(string query)
+        {
+
+            MySqlConnection connection = DBUtils.getConnection();
+
+            connection.Open();
+
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+        }
+
+    }
+
+}
---
+++ Lib/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lib
+{
+    public class Utils
+    {
+
+        public static string getHash(string arg)
+        {
+
+            MD5 md5 = MD5.Create();
+
+            string hash = Convert.ToBase64String(md5.ComputeHash(Encoding.UTF8.GetBytes(arg)));
+
+            return hash;
+
+        }
+
+    }
+
+}
